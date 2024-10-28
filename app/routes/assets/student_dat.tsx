f0ed6753dd41277e2e1cf758db0870ff83e04310
mikatan <gp22a150@oecu.jp>
2024-10-28 15:40:36 +0900

    class id をstringに変更

--- app/routes/assets/student_dat.tsx
+++ app/routes/assets/student_dat.tsx
@@ -1,32 +1,32 @@
 
 const studentDat : { [key: string]: Set<number> } = {}
 
-export async function isValidUsr(id: number | undefined, classid: number | undefined){
+export async function isValidUsr(id: number | undefined, classid: string | undefined){
     console.log(id, classid)
     if (id === undefined || classid === undefined) {
         return false
     }
     else{
-        if (studentDat[classid.toString()] === undefined) {
+        if (studentDat[classid] === undefined) {
             return false
         }
         else{
-            return studentDat[classid.toString()].has(id)
+            return studentDat[classid].has(id)
         }
     }
 }
 
-export async function pushUsr(id: number | undefined, classid: number | undefined){
+export async function pushUsr(id: number | undefined, classid: string | undefined){
     if (id === undefined || classid === undefined || id === 0) {
         return false
     }
     else{
         // クラスidが存在していないならクラスを生成する
-        if (studentDat[classid.toString()] === undefined) {
-            studentDat[classid.toString()] = new Set()
+        if (studentDat[classid] === undefined) {
+            studentDat[classid] = new Set()
         }
         // クラスidが存在しているならクラスに追加する
-        studentDat[classid.toString()].add(id)
+        studentDat[classid].add(id)
         return true
     }
 }
--- app/routes/student_dat.ts
+++ app/routes/student_dat.ts
@@ -4,7 +4,7 @@ import { isValidUsr, pushUsr } from "./assets/student_dat";
 export async function loader({ request }: any) {
     const url = new URL(request.url);
     const usrId = Number(url.searchParams.get("usr_id"));
-    const classId = Number(url.searchParams.get("class_id"));
+    const classId = url.searchParams.get("class_id")?.toString();
 
     const isValid = await isValidUsr(usrId, classId);
     return json({ isValid });
@@ -13,7 +13,7 @@ export async function loader({ request }: any) {
 export async function action({ request }: any) {
     const formData = await request.formData();
     const usrId = Number(formData.get("usr_id"));
-    const classId = Number(formData.get("class_id"));
+    const classId = formData.get("class_id")?.toString();
 
     await pushUsr(usrId, classId);
 }
