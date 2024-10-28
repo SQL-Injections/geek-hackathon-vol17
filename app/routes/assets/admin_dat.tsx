b3c76e9d4cbff031125442c391a6aae036f586c9
sleeping_mikan <gp22a150@oecu.jp>
2024-10-28 20:04:34 +0900

    命名の変更

--- app/routes/assets/admin_dat.tsx
+++ app/routes/assets/admin_dat.tsx
@@ -1,31 +1,31 @@
-const teacherDat : {[key: string]: {"password": string, "classes": Set<string>}} = {}
+const adminDat : {[key: string]: {"password": string, "classes": Set<string>}} = {}
 
 export function isValidUsr(usrId: string, password: string) {
     // ユーザがいなければ
-    if (teacherDat[usrId] === undefined) {
+    if (!adminDat[usrId]) {
         return false
     }
     //パスワードが一致するなら
-    return (teacherDat[usrId]["password"] === password) 
+    return (adminDat[usrId]["password"] === password) 
 }
 
 export function pushUsr(usrId: string, password: string) {
     // 既にユーザ名が存在する場合はfalseを返す
-    if (teacherDat[usrId] !== undefined) {
+    if (adminDat[usrId]) {
         return false
     }
 
     // データ生成
-    teacherDat[usrId] = {"password": password, "classes": new Set()}
+    adminDat[usrId] = {"password": password, "classes": new Set()}
     return true
 }
 
 export function addClass(usrId: string, password: string, classId: string) {
     //ユーザが存在しないならエラー
-    if (teacherDat[usrId] === undefined) {
+    if (!adminDat[usrId]) {
         return false
     }
     // データ生成
-    teacherDat[usrId]["classes"].add(classId)
+    adminDat[usrId]["classes"].add(classId)
     return true
 }
