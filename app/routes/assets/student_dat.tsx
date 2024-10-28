4ffef08201764665929d8481036e2982fd230670
mikatan <a200401021236@gmail.com>
2024-10-28 13:20:00 +0900

    student_dat(存在確認/データ追加)の実装

--- app/routes/assets/student_dat.tsx
+++ app/routes/assets/student_dat.tsx
@@ -1,8 +1,33 @@
+
+const studentDat : { [key: string]: Set<number> } = {}
+
 export async function isValidUsr(id: number | undefined, classid: number | undefined){
-    return true
+    console.log(id, classid)
+    if (id === undefined || classid === undefined) {
+        return false
+    }
+    else{
+        if (studentDat[classid.toString()] === undefined) {
+            return false
+        }
+        else{
+            return studentDat[classid.toString()].has(id)
+        }
+    }
 }
 
 export async function pushUsr(id: number | undefined, classid: number | undefined){
-    return true
+    if (id === undefined || classid === undefined || id === 0) {
+        return false
+    }
+    else{
+        // クラスidが存在していないならクラスを生成する
+        if (studentDat[classid.toString()] === undefined) {
+            studentDat[classid.toString()] = new Set()
+        }
+        // クラスidが存在しているならクラスに追加する
+        studentDat[classid.toString()].add(id)
+        return true
+    }
 }
 
