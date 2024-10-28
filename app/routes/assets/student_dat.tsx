226f3bdc79bee0a3bb7d7039c21897cdbdcc0454
sleeping_mikan <gp22a150@oecu.jp>
2024-10-28 20:07:12 +0900

    記法の変更

--- app/routes/assets/student_dat.tsx
+++ app/routes/assets/student_dat.tsx
@@ -3,11 +3,11 @@ const studentDat : { [key: string]: Set<number> } = {}
 
 export async function isValidUsr(id: number | undefined, classid: string | undefined){
     console.log(id, classid)
-    if (id === undefined || classid === undefined) {
+    if (!id || !classid) {
         return false
     }
     else{
-        if (studentDat[classid] === undefined) {
+        if (!studentDat[classid]) {
             return false
         }
         else{
@@ -17,12 +17,12 @@ export async function isValidUsr(id: number | undefined, classid: string | undef
 }
 
 export async function pushUsr(id: number | undefined, classid: string | undefined){
-    if (id === undefined || classid === undefined || id === 0) {
+    if (!id || !classid) {
         return false
     }
     else{
         // クラスidが存在していないならクラスを生成する
-        if (studentDat[classid] === undefined) {
+        if (!studentDat[classid]) {
             studentDat[classid] = new Set()
         }
         // クラスidが存在しているならクラスに追加する
