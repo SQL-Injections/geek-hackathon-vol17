868f280a6980bab39afcc78a3b749de78ea75587
s2108tomoka <163800046+s2108tomoka@users.noreply.github.com>
2024-10-30 14:57:47 +0900

    Classobjの型変更

--- app/routes/assets/class_dat.tsx
+++ app/routes/assets/class_dat.tsx
@@ -1,20 +1,14 @@
 
 // 連想配列を定義
-let Classobj : {[key:string]:number[][]}={
+let Classobj : {[key:string]:Array<Array<number>>}={
     "1": [[1,1,0],[0,1,1],[1,0,1]]
     // "ClassID": "座席のデータ"
 };
   
 export function isToClassSeats(id: string){
-    console.log("isToClassSeats : id = "+id);
-    console.log("Classobj[id] = "+ Classobj[id])
-    console.log("Classobj[id].type = "+ typeof(Classobj[id]))
-    Classobj[id].forEach(function( value ) {
-    
-        //配列の各要素を2倍にする
-        console.log(value);
-    
-    });
+    // Classobj[id].forEach(function( value ) {
+    //     console.log(value);
+    // });
     return Classobj[id];
 }
 
@@ -23,6 +17,5 @@ export function pushIdAndClass(id: string, seats:Array<Array<number>>){
     // console.log(seats);
     Classobj[id]=seats;
     console.log("class add ",Classobj[id]);
-    // ここで連想配列作ってマージさせたい
     return true
 }
