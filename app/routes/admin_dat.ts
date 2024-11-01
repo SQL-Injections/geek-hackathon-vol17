eb99ba80dd39cd663ec8a61f526da613c5fe80ff
s2108tomoka <s2108.ochitomoka@gmail.com>
2024-11-01 21:45:43 +0900

    admin_login実装

--- app/routes/admin_dat.ts
+++ app/routes/admin_dat.ts
@@ -3,25 +3,32 @@ import { isValidUsr, pushUsr, addClass } from "./assets/admin_dat";
 
 export async function loader({ request }: any) {
     const url = new URL(request.url);
+    console.log("url : ",url)
     const usrId = url.searchParams.get("usr_id")?.toString();
+    console.log("usrId : ",usrId);
     //いったんハッシュ化もソルトも存在しない状態で使うものとする
     const password = url.searchParams.get("password")?.toString();
+    console.log(password)
     
-    return json({item:false});
+    return json({item: await isValidUsr(usrId,password)});
 
 }
 
 export async function action({ request }: any) {
     const formData = await request.formData();
+    console.log(formData);
     const usrId = formData.get("usr_id")?.toString();
     const classId = formData.get("class_id")?.toString();
     const password = formData.get("password")?.toString();
     const func = formData.get("function");
 
     if (func === "pushUsr") {
+        console.log("pusher")
+        console.log(json({ pushUsr: await pushUsr(usrId, password) }));
         return json({ pushUsr: await pushUsr(usrId, password) });
     }
     if (func === "addClass") {
+        console.log("addClass")
         return json({ addClass: await addClass(usrId, password, classId) });
     }
     return json({ item: false });
--- app/routes/assets/admin_dat.tsx
+++ app/routes/assets/admin_dat.tsx
@@ -2,28 +2,36 @@ export const adminDat: { [key: string]: { password: string; classes: Set<string>
 
 export function isValidUsr(usrId: string | undefined, password: string | undefined) {
     if (!usrId || !password) {
+        console.log("(!usrId || !password)")
         return false
     }
     // ユーザがいなければ
     if (!adminDat[usrId]) {
+        console.log("(!adminDat[usrId])")
+        console.log("usr : ",adminDat[usrId])
         return false
     }
     //パスワードが一致するなら
+    console.log("passの一致 : ",adminDat[usrId]['password'] === password)
     return adminDat[usrId]['password'] === password
 }
 
 // createUser
 export function pushUsr(usrId: string | undefined, password: string | undefined) {
     if (!usrId || !password) {
+        console.log("(!usrId || !password)")
         return false
     }
     // 既にユーザ名が存在する場合はfalseを返す
     if (adminDat[usrId]) {
+        console.log("(adminDat[usrId])")
+        console.log((adminDat[usrId]))
         return false
     }
 
     // データ生成
     adminDat[usrId] = { password: password, classes: new Set() }
+    console.log(adminDat[usrId])
     return true
 }
 
